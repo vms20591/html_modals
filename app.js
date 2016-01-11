756123c05a8850dc9a9f3c8c40f8c15ce9f25792
Meenakshi Sundaram <vms20591@gmail.com>
2016-01-12 02:02:34 +0530

    formatted code and fixed code issue

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-function showModal() {
-    var id1 = document.getElementById("id1");
-    var id2 = document.getElementById("id2");
- 
-    id2.style.width = window.innerWidth + "px";
-    id2.style.height = window.innerHeight + "px";
-	id2.style.display = 'block';
-	 
-    $(id1).slideToggle(500, function() {
-        $(id1).css({
-            'top': '50%',
-            'margin-top': '-200px'
-        });
-    });
-}
+$(document).ready(function(){
 
-function hideModal() {
-    var id1 = document.getElementById("id1");
-    var id2 = document.getElementById("id2");
-    
-    $(id1).slideToggle(500, function() {
-        id2.style.display = 'none';
-    });
-}
+	window.showModal=showModal;
+	window.hideModal=hideModal;
+
+	function showModal() {
+		var id1 = document.getElementById("id1");
+		var id2 = document.getElementById("id2");
+	 
+		id2.style.width = window.innerWidth + "px";
+		id2.style.height = window.innerHeight + "px";
+		id2.style.display = 'block';
+		 
+		$(id1).slideToggle(500, function() {
+		    $(id1).css({
+		        'top': '50%',
+		        'margin-top': '-200px'
+		    });
+		});
+	}
+
+	function hideModal() {
+		var id1 = document.getElementById("id1");
+		var id2 = document.getElementById("id2");
+		
+		$(id1).slideToggle(500, function() {
+		    id2.style.display = 'none';
+		});
+	}
+});
